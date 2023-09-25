0b3aad397214297457390a14759792dd1ff9373f
Hack-it-Rafi <zozorafi2016@gmail.com>
2023-09-25 13:53:07 +0600

    Local Storage handled for given donations

--- My-Donation-Campaign/src/Components/Donation.jsx
+++ My-Donation-Campaign/src/Components/Donation.jsx
@@ -1,7 +1,17 @@
+import { useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
+import { getStoredDonations } from "./LocalStorage";
+
 const Donation = () => {
+    const data = useLoaderData();
+    const [donations, setDonations] = useState([]);
+    useEffect(()=>{
+        const storedDonations = getStoredDonations();
+        setDonations(storedDonations);
+    },[])
     return (
         <div>
-            This is Donation
+            <h2>Total Donation: {donations.length}</h2>
         </div>
     );
 };
--- My-Donation-Campaign/src/Components/DonationDetails.jsx
+++ My-Donation-Campaign/src/Components/DonationDetails.jsx
@@ -1,4 +1,5 @@
 import { useLoaderData, useParams } from "react-router-dom";
+import { saveDonations } from "./LocalStorage";
 
 const DonationDetails = () => {
     const cards = useLoaderData();
@@ -6,13 +7,16 @@ const DonationDetails = () => {
     console.log(id);
     const card = cards.find(card => card.id == id);
     console.log(card);
+    const handleDonate=()=>{
+        saveDonations(parseInt(id));
+    }
     return (
         <div className="mt-10">
             <div className="z-10 relative">
                 <img className="w-full rounded-lg md:h-[650px]" src={card.image} alt="" />
                 <div className="absolute bottom-0 w-full">
                     <div className="bg-black bg-opacity-50 py-10 px-10 rounded-b-lg">
-                        <button className="py-2 px-4  text-center rounded-lg text-white text-lg" style={{ backgroundColor: `${card.text_color}` }}>Donate {card.price}</button>
+                        <button onClick={handleDonate} className="py-2 px-4  text-center rounded-lg text-white text-lg" style={{ backgroundColor: `${card.text_color}` }}>Donate {card.price}</button>
                     </div>
                 </div>
             </div>
